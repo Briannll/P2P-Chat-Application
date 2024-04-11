abe679ad31e688724a8974e07e7835b1ebd51e8e
Brian Lai <150647749+Briannll@users.noreply.github.com>
2024-04-11 00:20:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
+const App = () => {
+  return (
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+        {/* NavBar */}
+        {/* Chat Component */}
+      </section>
+    </div>
+  );
+};
+
+export default App;
